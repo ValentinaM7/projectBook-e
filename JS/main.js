8eb24da430a3b6cdaf1a2f64fc7b013f47ee67e6
Valentina Meyer <valemeyer@yahoo.com.ar>
2025-09-04 19:55:57 +1000

    render categorias

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,13 @@
 const categories = document.getElementsByClassName("categories-btn");
 const aside = document.getElementById("myAside");
 const cerrarBtn = document.getElementById("close");
-const informacionLibros = document.getElementById("book-detailsjs");
 const openBtn = document.getElementById("openBtn");
 const detailsaside = document.getElementById("aside-detalles");
 const carroFisico = document.getElementById("carrito-fisico");
 const listadeCompra = document.getElementById("lista-compra");
 const cerrarcarro = document.getElementById("cerrar-carrito");
 const listacarro = document.getElementById("list-cart");
+const categorias = document.getElementById("lista-categorias");
 
 openBtn.addEventListener("click", () => {
   aside.classList.add("show");
@@ -210,10 +210,60 @@ const books = [
   },
 ];
 
+const getCategoriasUnicas = () => {
+  const categoriasLibros = books.map((book) => book.category); //Traemos todas las categorias a una nueva lista array
+
+  let setDeElementos = new Set(categoriasLibros); //Hacemos el set con categorias que no sean repetidas
+
+  const ArrayCategorias = Array.from(setDeElementos); //las transformamos en array
+
+  categorias.innerHTML = ""; //limpiamos y despues renderizamos
+
+  ArrayCategorias.forEach((cat) => {
+    categorias.innerHTML += `
+    <li class="cat-item">
+      <button class="categories-btn" data-cat="${cat}">${cat}</button>
+    </li>
+  `;
+  });
+
+  const BotonesCat = document.querySelectorAll(".categories-btn");// Selecciono TODOS los botones de categoría
+
+  BotonesCat.forEach((btn) => { // A cada botón le agrego su listener
+    btn.addEventListener("click", () => {
+      const categoriaSeleccionada = btn.dataset.cat; // data-cat es donde el botón guarda escondida su categoría, una mochilita
+      showBooksOnlyCat(categoriaSeleccionada);
+    });
+  });
+
+  
+  const showBooksOnlyCat = (bookCat) => { // Función para mostrar los libros filtrados
+    const librosFiltrados = books.filter((b) => b.category === bookCat);
+
+    openBtn.innerHTML = ""; // limpio antes de imprimir
+
+    librosFiltrados.forEach((book) => {
+      openBtn.innerHTML += `
+      <div class="libro-picandtext" id=${book.id} onclick="showBookDetails(${book.id})">
+        <img src="${book.image}" alt="${book.title}">
+        <div class="libro-detalles">
+          <h3>${book.title}</h3>
+          <p class="author">${book.author}</p>
+          <p class="pages">${book.pages} pages</p>
+          <p class="price">$${book.price}</p>
+        </div>
+      </div>
+    `;
+    });
+  };
+};
+
 const templateLibros = () => {
-  books.forEach((book) => {
-    openBtn.innerHTML += `<div class="libro-picandtext" id=${book.id} onclick="showBookDetails(${book.id})">
-                            <img ${book.image} alt="${book.title} ">
+  books
+    .filter((book) => book.price < 11)
+    .forEach((book) => {
+      openBtn.innerHTML += `<div class="libro-picandtext" id=${book.id} onclick="showBookDetails(${book.id})">
+                            <img src =${book.image} alt="${book.title} ">
                             <div class="libro-detalles">
                                 <h3>${book.title} </h3>
                                 <p class="author">${book.author} </p>
@@ -221,7 +271,7 @@ const templateLibros = () => {
                                 <p class="price"> $${book.price} </p></p>
                             </div>
                         </div>`;
-  });
+    });
 };
 
 const agregadoraDeLibros = () => {
@@ -234,7 +284,16 @@ const agregadoraDeLibros = () => {
               <button class="remove">-</button>
                 </div>`;
   });
-};
+  
+  // const borrar = document.querySelectorAll ('.remove');
+  //   borrar.forEach((botonborrar) =>{
+  //     botonborrar.addEventListener("click", (e) => {
+  //       const ElementoABorrar = e.target.ParentElement;
+  //       ElementoABorrar.toggle();   
+  //     });
+  //   });
+  }
+
 
 const showBookDetails = (bookId) => {
   const book = books.find((b) => b.id === bookId); // Encuentra el libro con el id correspondiente
@@ -258,8 +317,6 @@ const showBookDetails = (bookId) => {
     //ejecutamos las dos funciones juntas ya que innerhtml es sincrono en codigo
 
     const botonAdd = document.getElementsByClassName("add");
-    
-
 
     const arrayBotonesAdd = Array.from(botonAdd);
 
@@ -275,29 +332,42 @@ const showBookDetails = (bookId) => {
   });
 
   const compra = document.getElementById("ya");
-  compra.addEventListener("click", () => {
+  compra.addEventListener("click", async () => {
     Swal.fire({
-      title: "Drag me!",
-      icon: "success",
-      draggable: true,
+      title: "Type your credit card details",
+      html: `
+    <input type="text" 
+           id="idInput" 
+           pattern="[0-9]{12}">
+  `,
+      showCancelButton: true,
+      confirmButtonText: "Save",
+      confirmButtonColor: "#2FC2AD",
+      cancelButtonText: "Cancel",
+      cancelButtonColor: "#0e3d36",
+      preConfirm: () => {
+        const input = document.getElementById("idInput");
+        if (!input.value || !input.validity.valid) {
+          Swal.showValidationMessage("Please 12 use numbers");
+          return false; // Evita que se cierre el modal si no es válido
+        }
+        return input.value;
+      },
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "You bought a new book!",
+          text: "We will send you an email with the tracking number",
+          icon: "success",
+          confirmButtonText: "Great!",
+          confirmButtonColor: "#2FC2AD",
+        });
+      }
     });
   });
-
-  // // botonAdd.forEach = (boton => {
-  // //   botonAdd.addEventListener("click", () => {
-  // //     Swal.fire({
-  // //       position: "center",
-  // //       width: "75%",
-  // //       toast: true,
-  // //       icon: "success",
-  // //       iconColor: "#2FC2AD",
-  // //       title: "You added a new book",
-  // //       showConfirmButton: false,
-  // //       timer: 1200,
-  // //     });
-  // //   });
 };
 templateLibros();
+getCategoriasUnicas();
 
 // document.addEventListener("DOMContentLoaded", async () => {
 //     try{
